2e008ed87bc6ed5bbaa14d434f95215ce641c573
YousefKurchaloy <yousef.shaher@gmail.com>
2024-09-22 11:28:26 +0300

    Added header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,16 @@
 /*   By: yalshish <yalshish@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 13:34:37 by yalshish          #+#    #+#             */
-/*   Updated: 2024/09/21 13:34:38 by yalshish         ###   ########.fr       */
+/*   Updated: 2024/09/22 11:27:40 by yalshish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+char *get_next_line(int fd);
+
+#endif
