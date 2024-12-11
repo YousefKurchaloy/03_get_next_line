1d0189db543e115ddf319dd216a9c14a5d5d1824
YousefKurchaloy <yousef.shaher@gmail.com>
2024-12-12 01:27:58 +0300

    added documentation

--- get_next_line.c
+++ get_next_line.c
@@ -3,24 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kurchaloy <kurchaloy@student.42.fr>        +#+  +:+       +#+        */
+/*   By: yalshish <yalshish@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 15:15:35 by yalshish          #+#    #+#             */
-/*   Updated: 2024/12/11 12:26:36 by kurchaloy        ###   ########.fr       */
+/*   Updated: 2024/12/12 01:27:49 by yalshish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+// This function is used to free the address:
+// of the pointer passed as an argument.
+// and set the pointer to NULL.
+// It has a return type of void* to be able to return NULL.
 void	*ft_free(char **str)
-// this function is used to free the memory allocated by reference
-// to the pointer passed as an argument and set the pointer to NULL
 {
 	free(*str);
 	*str = NULL;
 	return (NULL);
 }
 
+// This function is used to find the first '\n' of a character in a string.
+// then subtract the address of the first character in the string
+// from the address of the first occurrence of '\n'.
+// then +1 to include the '\n' character.
+// this method of subtraction is used to find the length of the string.
+// which is much faster than using strlen.(itterating through the string)
+// if the character at index len is not found, the function returns NULL.
+// and frees the memory of line.
+// otherwise, it returns new_line.
+// new_line is the line from the '\n' to the next '\n'.
+// then the line is freed. And the new_line is returned.
 char	*clean_storage(char *line)
 {
 	char	*new_line;
@@ -44,7 +57,16 @@ char	*clean_storage(char *line)
 	return (new_line);
 }
 
-char	*new_buf(char *line)
+// This function creates a new buffer of size len.
+// len is the length that was found by subtracting addresses.
+// The addresses subtracted:
+// The address of the first occurrence of a newline character.
+// The address of the first character in the line.
+// The + 1 is to include the newline character.
+// Then using the ft_substr function to copy the line into buf.
+// If the allocation fails, the function returns NULL.
+// Otherwise, it returns buf.
+char	*new_buffer(char *line)
 {
 	char	*buf;
 	char	*ptr;
@@ -58,31 +80,48 @@ char	*new_buf(char *line)
 	return (buf);
 }
 
+// This function reads from the file descriptor and returns the line.
+// It allocates a buffer of size BUFFER_SIZE + 1.
+// The buffer is initialized to an empty string.
+// The function reads from the file descriptor until it reaches '\n'.
 char	*read_buffer(int fd, char *line)
 {
-	int		byte;
+	int		bytes_read;
 	char	*buffer;
 
-	byte = 1;
-	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	bytes_read = 1;
+	buffer = malloc(BUFFER_SIZE + 1);
 	if (!buffer)
 		return (ft_free(&line));
 	buffer[0] = '\0';
-	while (byte > 0 && !ft_strchr(buffer, '\n'))
+	while (bytes_read > 0 && !ft_strchr(buffer, '\n'))
 	{
-		byte = read (fd, buffer, BUFFER_SIZE);
-		if (byte > 0)
+		bytes_read = read (fd, buffer, BUFFER_SIZE);
+		if (bytes_read > 0)
 		{
-			buffer[byte] = '\0';
+			buffer[bytes_read] = '\0';
 			line = ft_strjoin(line, buffer);
+			if (!line)
+				break ;
 		}
 	}
 	free(buffer);
-	if (byte == -1)
+	if (bytes_read == -1)
 		return (ft_free(&line));
 	return (line);
 }
 
+// This function reads the file descriptor and returns the next line.
+// File descriptors are non-negative unique integers.
+// The operating system assigns the lowest available file descriptor
+// to a newly opened file.
+// The file descriptor is used by the program
+// to interact with files, sockets, input/output (I/O) resources,
+// when performing read, write, or other operations.
+// The function initializes a static variable line to NULL.
+// Static variable meaning that,
+// its lifetime is the entire run of the program.
+// The function calles read_buffer to read from the file descriptor
 char	*get_next_line(int fd)
 {
 	static char	*line = {NULL};
@@ -94,7 +133,7 @@ char	*get_next_line(int fd)
 		line = read_buffer (fd, line);
 	if (!line)
 		return (NULL);
-	buf = new_buf(line);
+	buf = new_buffer(line);
 	if (!buf)
 		return (ft_free(&line));
 	line = clean_storage(line);
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,27 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kurchaloy <kurchaloy@student.42.fr>        +#+  +:+       +#+        */
+/*   By: yalshish <yalshish@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 15:15:56 by yalshish          #+#    #+#             */
-/*   Updated: 2024/12/11 10:57:45 by kurchaloy        ###   ########.fr       */
+/*   Updated: 2024/12/11 23:06:52 by yalshish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+// This function counts the number of characters in the string s.
+// It returns 0 if the string is NULL.
+// else it iterates through the string and counts the characters.
+// till it reaches the null terminator.
 size_t	ft_strlen(const char *s)
 {
 	size_t	i;
 
-	i = 0;
 	if (!s)
 		return (0);
+	i = 0;
 	while (s[i])
 		i++;
 	return (i);
 }
 
+// This function searches for the first occurrence of the character c.
+// It returns a pointer to the character
+// or NULL if the character is not found.
 char	*ft_strchr(const char *s, int c)
 {
 	while (*s != (unsigned char)c)
@@ -32,28 +39,40 @@ char	*ft_strchr(const char *s, int c)
 	return ((char *)s);
 }
 
+// This function creates a new string from the string s.
+// It returns NULL if the string s is NULL or if the length is 0,
+// or if the start index is greater than the length of the string.
+// It starts from the index start and copies len characters.
+// If the length of the string is less than len:
+// it will copy the whole string.
+// else:
+// it will copy len characters.
+// + 1 for the null terminator.
+// It returns the new string.
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*res;
 	char	*src;
 	size_t	reslen;
 
-	if (!s)
+	if (!s || ft_strlen(s) < (size_t)start || len == 0)
 		return (NULL);
-	if (ft_strlen(s) < (size_t)start)
-		return (NULL);
-	src = (char *)s + start;
+	src = s + start;
 	if (ft_strlen(src) < len)
 		reslen = ft_strlen(src) + 1;
 	else
 		reslen = len + 1;
-	res = malloc(reslen * sizeof(char));
+	res = malloc(reslen);
 	if (!res)
 		return (NULL);
 	ft_strlcpy(res, src, reslen);
 	return (res);
 }
 
+// This function copies the string src to the string dst.
+// It copies at most (size - 1) characters from src to dst.
+// If the size is 0 it will return the length of the string src.
+// It returns the length of the string src.
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
@@ -61,40 +80,49 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	i = 0;
 	if (size == 0)
 		return (ft_strlen(src));
-	while (src[i] != '\0' && i < size - 1)
+	while (src[i] && i < (size - 1))
 	{
 		dst[i] = src[i];
 		i++;
 	}
 	dst[i] = '\0';
-	while (src[i] != '\0')
+	while (src[i])
 		i++;
 	return (i);
 }
 
+// This function joins 2 strings together,
+// it only returns NULL when the malloc fails.
+// It also reproduces the behaviour of ft_strdup("")
+// where if the string is NULL it will allocate 2 bytes to make an empty string.
+// The allocation is for:
+// Sum of the lengths of the 2 strings + 1 for the null terminator.
+// If the allocation fails it will free the first string and return NULL.
+// It also frees the first string passed as an argument.
+// It returns the new string.
 char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*str;
 	size_t	i;
-	size_t	c;
+	size_t	j;
 
 	if (!s1)
 	{
-		s1 = malloc(sizeof(char) + 1);
+		s1 = malloc(2);
 		if (!s1)
 			return (NULL);
 		s1[0] = 0;
 	}
-	str = (char *)malloc(sizeof(char) * ft_strlen(s1) + ft_strlen(s2) + 1);
+	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!str)
 		return (ft_free(&s1));
 	i = -1;
 	while (s1[++i])
 		str[i] = s1[i];
-	c = -1;
-	while (s2[++c])
-		str[i + c] = s2[c];
-	str[i + c] = '\0';
+	j = -1;
+	while (s2[++j])
+		str[i + j] = s2[j];
+	str[i + j] = '\0';
 	free(s1);
 	return (str);
 }
